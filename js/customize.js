348815989e26a202d81dd9c1f10b94d795a333b8
Kai Fu Hsieh <eason@tpe.hyweb.com.tw>
2019-08-15 17:56:41 +0800

    系統設定初版release

--- js/customize.js
+++ js/customize.js
@@ -106,4 +106,17 @@ $(function() {
             });
         }
     });
+    rwdTable();
+    function rwdTable() {
+        $('.table_list').find('table').each(function() {
+            var $row = $(this).find('tr');
+            rowCount = $row.length;
+            for (var n = 1; n <= rowCount; n++) {
+                $(this).find('th').each(function(index) {
+                    var thText = $(this).text();
+                    $row.eq(n).find('td').eq(index).attr('data-title', thText);
+                });
+            }
+        });
+    }
 });
